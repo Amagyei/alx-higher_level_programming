bdb29e3115ae3f12632e027ed4a212140f8566f0
Nana Kwame Amagyei <nanakwameamagyei@Nanas-MacBook-Pro.local>
2024-01-04 01:40:01 +0000

    task 2 and 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ['e', 'q']:
-        print("{}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
