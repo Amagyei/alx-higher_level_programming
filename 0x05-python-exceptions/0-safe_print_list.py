1eab8f6f56495a90eb4814ca201e8a197065dea3
amagyei <kwame.na@hotmail.com>
2024-01-22 19:23:55 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list:
+            print("{:d}".format(i)
+            count++
+            if count >= x:
+                break
+    except Exception as e:
+        print('an error occured')
+        
