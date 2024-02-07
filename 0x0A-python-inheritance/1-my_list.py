ff8bcccaccde9d11bcb7d83f98cbea0b1587349d
amagyei <kwame.na@hotmail.com>
2024-02-07 02:26:53 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" inherits from list """
+
+def MyList(list):
+	""" class that inherits from list """
+	def print_sorted(self):
+		""" prints a list in sorted ascending order """
+		print(sorted(self))
+
