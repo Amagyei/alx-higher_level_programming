19c7b3f463460744cc3e311e861f5e9d53f39783
amagyei <kwame.na@hotmail.com>
2024-02-11 23:27:19 +0000

    task 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def base(self):
+	__nb_objects = 0
+	
+	def __init__(self, id=None):
+		if id != None:
+			self.id = id
+		else:
+			__nb_objects++
+			self.id = __nb_objects
+	
+
