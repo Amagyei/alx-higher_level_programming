46e34e0a25a891a23bb249a5af6720a83ef54a0d
amagyei <kwame.na@hotmai.com>
2024-04-14 13:56:11 -0700

    task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import mySQLdb
+import sys
+if __name__ = '__main__':
+    conn = MySQLdb.connect(
+            host ="localhost",
+            port = 3306,
+            user = argv[1],
+            passwd = argv[2],
+            db =argv[3],
+            charset = 'utf8')
+    cur = conn.cursor()
+    try:
+        stmt ="SELECT * FROM states order by id ASC"
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+         try:
+            rtn = ("MySQLdb Error")
+        except p:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in return:
+        print(i)
+    cur.close()
+    conn.close()
+
+            
