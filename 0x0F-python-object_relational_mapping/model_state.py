94f5a62fb2d1b46199390008f906bec66ae8f6c1
amagyei <kwame.na@hotmai.com>
2024-04-14 21:34:01 -0700

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,14 @@
 """Start link class to table in database
 """
 import sys
-import model, base from model_state
+import model  from model_state
+import base from model_state
 
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], 
+        sys.argv[2], 
+        sys.argv[3]), 
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
