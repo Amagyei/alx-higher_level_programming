fb77949f2f9932d738336700e98ac5ba0c9ec220
amagyei <kwame.na@hotmail.com>
2024-04-28 08:36:28 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# taskes in a s=url sends the body of the request and displays blah blah blah
-
+# taskes in a s=url sends the body of the request and displays blah blah blah:wq
 curl -sL "$1"
-
