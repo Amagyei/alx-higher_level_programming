4008c087559752c3c5a9ff26746834999b16932d
amagyei <kwame.na@hotmail.com>
2024-09-09 23:06:15 +0000

    task 2 pycodestyle validation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,7 @@
 
 """ function that checks if object is instance """
 
+
 def is_same_class(obj, a_class):
-	""" function that checks if object is instance """
-	return type(obj) is a_class
+    """ function that checks if object is instance """
+    return type(obj) is a_class
