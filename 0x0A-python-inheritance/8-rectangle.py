f3947dc98532fa9db956eac29064d0be7e68e1ba
amagyei <kwame.na@hotmail.com>
2024-09-12 15:32:01 +0000

    task 7 and 8 pycodestyling

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,12 +4,13 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-	    """Initialize a new Rectangle instance."""
-	    self.integer_validator("width", width)
-	    self.integer_validator("height", height)
-	    self.__width = width
-	    self.__height = height
+        """Initialize a new Rectangle instance."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
