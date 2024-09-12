9de3103d1e335f6793e4a46baf703877955fb71a
amagyei <kwame.na@hotmail.com>
2024-09-12 16:04:45 +0000

    task 10

---
+++ 0x0A-python-inheritance/9-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""10-square.py"""
+
+Rectangle = __import_('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ inherits the rectagnle class """
+
+    def __init__(self, size):
+        """ init func for the obj"""
+        self.integer_validator = size
+        self.__size = size
+
+    def area(self):
+        """ Returns the area"""
+        return size * size
