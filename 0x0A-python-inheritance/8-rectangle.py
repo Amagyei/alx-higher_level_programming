92f188eb08887a2d14ccecdada2c10fc571e9919
amagyei <kwame.na@hotmail.com>
2024-09-12 15:11:40 +0000

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''REctangle'''
+
+
+from base_geometry import BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+	self.integer_validator("width", width)
+	self.integer_validator("height", height)
+	self.__width = width
+	self.__height = height 
+
+
