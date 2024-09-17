920501509f4705644e2462d465550d19bb56eb8c
amagyei <kwame.na@hotmail.com>
2024-09-17 23:20:56 +0000

    task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'local:Wq;wqhost';
