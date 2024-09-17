b3c0e185e53e1288d44132225bf0252c91846665
amagyei <kwame.na@hotmail.com>
2024-09-17 13:42:23 +0000

    task semistandard formatting for all files

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,28 @@
 #!/usr/bin/node
 // JS Script
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-         const character = 'X';
-        for (let i = 0; i < this.height; i++){
-            console.log(character.repeat(this.width));
-        }
+  print () {
+    const character = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
     }
+  }
 
-    double() {
-        this.width *= 2;
-        this.height *=2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 
-    rotate() {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-    }
-
-
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 };
-
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,11 @@
 
 const Square0 = require('./5-square');
 
-module.exports = class Square extends Square0{
-    charPrint(c) {
-        let char = ( c !== undefined) ? c: 'X'
-        for( let i = 0; i < this.height; i++){
-            console.log(char.repeat(this.width));
-        }
+module.exports = class Square extends Square0 {
+  charPrint (c) {
+    const char = (c !== undefined) ? c : 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
     }
-}
+  }
+};
