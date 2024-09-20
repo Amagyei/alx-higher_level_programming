7b96a35085826c400b36d80c914046330702daa0
amagyei <kwame.na@hotmail.com>
2024-09-20 15:26:29 -0700

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,22 @@ const args = process.argv.slice(2)
 
 const endpoint = 'https://swapi-api.alx-tools.com/api/people/18'
 
-request(endpoint, (err, response, body) => {
+request(endpoint, (args[0], response, body) => {
 	if (err) {
 		console.error(err);
 	} else { 
-		console.log(JSON.parse(body).films.length);
+		const results = JSON.parse.body.results;
+		let n = 0;
+
+		resutls.forEach(result => { 
+			const characters = result.characters;
+
+			characters.forEach(character => {
+				if(character.endsWith('18/')) {
+					n++;
+				}
+			});
+		 });
+		console.log(n)
 	}
 });
