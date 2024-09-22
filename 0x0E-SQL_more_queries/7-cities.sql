410fa83d0f93a0c7efb62c3409ddf6c8b10dc463
amagyei <kwame.na@hotmail.com>
2024-09-22 05:51:14 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,8 @@
 -- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
