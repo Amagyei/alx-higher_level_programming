7c12aed0978738e028389566ad5239e4ff162a6f
amagyei <kwame.na@hotmail.com>
2024-09-22 04:55:43 -0700

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Script that creates an user and a database in MySQL server
+-- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS force_name.hbtn_0d_usa(
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
