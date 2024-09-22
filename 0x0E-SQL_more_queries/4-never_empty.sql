02bcc03544eb73b3a8934a12c29248f09aef667b
amagyei <kwame.na@hotmail.com>
2024-09-22 04:44:37 -0700

    task 4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Script that creates an user and a database in MySQL server
+-- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
