ace450dc2b03249f8b271eb9337664004cd0bf10
amagyei <kwame.na@hotmail.com>
2024-09-22 05:16:13 -0700

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Script that creates an user and a database in MySQL server
+-- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
