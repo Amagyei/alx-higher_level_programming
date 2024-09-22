07fe93c03168054f9328909d8f6c6b130042f38a
amagyei <kwame.na@hotmail.com>
2024-09-22 16:41:25 -0700

    tasks 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- script that lists all shows with at least 1 genre
+-- query to select the shows form the database
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id; 
