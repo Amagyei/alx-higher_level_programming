5b761bfc7e408c8dd5a7751ffedf0838d51539ee
amagyei <kwame.na@hotmail.com>
2024-09-22 05:07:10 -0700

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,9 +2,7 @@
 -- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS force_name(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.orce_name(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
