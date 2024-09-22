e6675189dcbe9ff664d36d1490feaf973a917f87
amagyei <kwame.na@hotmail.com>
2024-09-22 04:49:20 -0700

    task 5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Script that creates an user and a database in MySQL server
+-- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT DEFAULT 1,
+	name VARCHAR(256) NOT NULL
+);
