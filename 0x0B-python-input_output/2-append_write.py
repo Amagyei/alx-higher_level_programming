12b14aaf3039ebe0dc987013c1f84dd0ce3564c2
amagyei <kwame.na@hotmail.com>
2024-09-27 10:23:07 -0700

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,13 @@
 """ Defines a function that reads a file"""
 
 def write_file(filename="",text=""):
-    """ pprints the contedts of a UTF8 text file to stdout """
+    """ pprints the contedts of a UTF8 text file to stdout 
+    
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
     with open(filename,'a', encoding="utf-8") as f:
         return f.write(text)
