d171a3e8b4ceb70d2a838f977629835a448a2dd1
amagyei <kwame.na@hotmail.com>
2024-09-27 10:28:24 -0700

    tas 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Defines a function that reads a file"""
 
-def write_file(filename="",text=""):
-    """ pprints the contedts of a UTF8 text file to stdout 
+def append_write(filename="",text=""):
+    """Appends a string to the end of a UTF8 text file.
     
     Args:
         filename (str): The name of the file to append to.
