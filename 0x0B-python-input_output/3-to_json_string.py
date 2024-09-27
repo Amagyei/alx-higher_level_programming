90e214dc21bb98f0654dd09ab19b6709436b34e3
amagyei <kwame.na@hotmail.com>
2024-09-27 10:32:44 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a string-to-JSON function."""
+import json
+
+def to_json_string(my_obj):
+    """ Returnd the json rep of a string """
+    return json.dumps.(my_obj)
